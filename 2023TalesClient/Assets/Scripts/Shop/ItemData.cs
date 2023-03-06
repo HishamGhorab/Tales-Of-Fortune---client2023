150c34da5b0983a79c6ee7ea070ce28bffe52018
Hadi Ghorab <hadi.ghorab@futuregames.nu>
2023-03-06 19:19:56 +0100

    Added UI

---
+++ 2023TalesClient/Assets/Scripts/Shop/ItemData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemData : MonoBehaviour
+{
+    public ItemData(Item item, int quantity) 
+    {
+        this.item = item; 
+        this.quantity = quantity; 
+        this.name = item.name;
+    }
+    
+    public string name; 
+    public Item item; 
+    public int quantity;
+}
